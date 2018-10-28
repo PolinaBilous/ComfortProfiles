b4c252195b36d69f968bae49a203dfd6d0108bd3
Polina Bilous <polina.bilous@nure.ua>
2018-10-28 22:00:26 +0300

    home, sign in and sign up pages implemented in front end

--- ComfortProfilesSharingFront/comfort-profiles-sharing/src/app/app-routing.module.ts
+++ ComfortProfilesSharingFront/comfort-profiles-sharing/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "sign-in", component: SignInComponent}, 
+  {path: "sign-up", component: SignUpComponent}, 
+  {path: "home", component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ComfortProfilesSharingFront/comfort-profiles-sharing/src/app/app.module.ts
+++ ComfortProfilesSharingFront/comfort-profiles-sharing/src/app/app.module.ts
@@ -6,16 +6,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiTestComponent } from './api-test/api-test.component';
 import { Service } from 'src/logic/service.service';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApiTestComponent
+    ApiTestComponent,
+    SignInComponent,
+    HomeComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
   ],
   providers: [Service],
   bootstrap: [AppComponent]
